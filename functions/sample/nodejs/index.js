7408ddb2560e5a915cf991cba51216fa4717bfa8
= <=>
2023-10-06 18:28:45 -0400

    parte4ProjetoFinal

--- functions/sample/nodejs/index.js
+++ functions/sample/nodejs/index.js
@@ -6,25 +6,24 @@
  const { IamAuthenticator } = require("ibm-cloud-sdk-core");
  
  function main(params) {
-   const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY });
-   const cloudant = CloudantV1.newInstance({
-     authenticator: authenticator,
-   });
-   cloudant.setServiceUrl(params.COUCH_URL);
- 
-   let dbList = getDbs(cloudant);
-   return { dbs: dbList };
+
+    const cloudant = Cloudant({
+        url: params.COUCH_URL,
+        plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
+    });
+
+    let dbListPromise = getDbs(cloudant);
+    return dbListPromise;
  }
  
  function getDbs(cloudant) {
-   cloudant
-     .getAllDbs()
-     .then((body) => {
-       body.forEach((db) => {
-         dbList.push(db);
-       });
-     })
-     .catch((err) => {
-       console.log(err);
-     });
+    return new Promise((resolve, reject) => {
+        cloudant.db.list()
+            .then(body => {
+                resolve({ dbs: body });
+            })
+            .catch(err => {
+                reject({ err: err });
+            });
+    });
  }
