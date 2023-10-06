1861edd75bf151ab8c341be6e858e07ff4b587ee
= <=>
2023-10-05 21:12:24 -0400

    parte4ProjetoFinal

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
 from . import views
 
 app_name = 'djangoapp'
@@ -20,7 +21,9 @@ urlpatterns = [
     # path for logout
 
     path(route='', view=views.get_dealerships, name='index'),
-
+    path(route='about', view=views.about, name='about'),
+    path('admin/', admin.site.urls),
+    path('djangoapp/', include('djangoap/urls.py')),
     # path for dealer reviews view
 
     # path for add a review view
