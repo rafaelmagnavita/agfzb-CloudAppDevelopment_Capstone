76bb50a9cef58feb03e028ec4c85a2da2c930fad
= <=>
2023-10-10 11:02:08 -0400

    parte4ProjetoFinal

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -26,7 +26,37 @@ class CarModel(models.Model):
         return f"{self.year} {self.car_make.name} {self.name}"
 
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
 
+    def __init__(self, _id, _rev, id, city, state, st, address, zip, lat, long, full_name, short_name):
+        # Dealer id
+        self._id = _id
+        # Dealer rev
+        self._rev = _rev
+        # Dealer id
+        self.id = id
+        # Dealer city
+        self.city = city
+        # Dealer state
+        self.state = state
+        # Dealer state
+        self.st = st
+        # Dealer address
+        self.address = address
+        # Dealer zip
+        self.zip = zip
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer short name
+        self.short_name = short_name
+
+
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
