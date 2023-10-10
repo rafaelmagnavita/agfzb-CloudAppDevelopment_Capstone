9067a3c0ad4ea66b5fd4cbc2183fae37bc844ff4
= <=>
2023-10-10 11:33:16 -0400

    parte4ProjetoFinal

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-from .models import CarDealer
+from .models import CarDealer, DealerReview
 from .restapis import get_dealers_from_cf,get_request
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
